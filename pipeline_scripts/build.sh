f72d17cef71bd70b47409097d7880f62ff83ba81
Ahmed Elsabbahy <elsabbahyahmed@yahoo.com>
2015-04-23 17:46:13 -0400

    bumped Android version

--- pipeline_scripts/build.sh
+++ pipeline_scripts/build.sh
@@ -17,7 +17,7 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
   echo y | android update sdk --no-ui --filter build-tools-20.0.0 --all > /dev/null
 
   # The SDK version used to compile your project
-  echo y | android update sdk --no-ui --filter android-18 > /dev/null
+  echo y | android update sdk --no-ui --filter android-22 > /dev/null
 
   # uncomment to install the Extra/Android Support Library
   # echo y | android update sdk --no-ui --filter extra-android-support --all > /dev/null
